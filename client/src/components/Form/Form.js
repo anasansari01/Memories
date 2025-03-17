bd140b6553bf1024ec702df8e26fca4e9a19ca56
anasansari01 <anasansari056789@gmail.com>
2025-03-17 20:27:28 +0530

    Add the clear button

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,8 +17,8 @@ const Form = ({ currentId, setCurrentId }) => {
   }, [post]);
 
   const clear = () => {
-    // setCurrentId(0);
-    // setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
   };
 
   const handleSubmit = async (e) => {
@@ -35,12 +35,13 @@ const Form = ({ currentId, setCurrentId }) => {
     //   dispatch(updatePost(currentId, postData));
     //   clear();
     // }
+    clear();
   };
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">Creating a Memory.</Typography>
+        <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
