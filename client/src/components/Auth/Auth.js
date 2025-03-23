7fa11b247d3817a0bee1ea9d6abd959e0863489e
Anas Ansari <anasansari056789@gmail.com>
2025-03-23 20:38:33 +0530

    Add the full authentication System with JWT and Google Login

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import { GoogleLogin } from 'react-google-login';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
+import { signin, signup } from '../../actions/auth';
 import Icon from './icon';
 import { AUTH } from '../../constants/actionTypes';
 import useStyles from './styles';
@@ -12,7 +14,7 @@ import Input from './Input';
 
 const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
 
-const SignUp = () => {
+const Auth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
   const dispatch = useDispatch();
@@ -23,17 +25,49 @@ const SignUp = () => {
   const handleShowPassword = () => setShowPassword(!showPassword);
 
   const switchMode = () => {
-    setIsSignup((prevIsSignUp) => (!prevIsSignUp));
-    handleShowPassword(false);
+    setForm(initialState);
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
   };
 
-  const handleSubmit = () => { };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const googleError = () => { };
+    if (isSignup) {
+      dispatch(signup(form, history));
+    } else {
+      dispatch(signin(form, history));
+    }
+  };
 
-  const handleChange = () => {
+  const googleSuccess = async (res) => {
+    try {
+      const decoded = jwtDecode(res.credential);
+      console.log('Decoded User Info:', decoded);
 
-  }
+      const result = {
+        name: decoded.name,
+        email: decoded.email,
+        googleId: decoded.sub,
+        imageUrl: decoded.picture,
+      };
+
+      const token = res.credential;
+
+      dispatch({ type: AUTH, data: { result, token } });
+      history.push('/');
+    } catch (error) {
+      console.log('Error decoding Google credential:', error);
+    }
+  };
+
+  const googleError = (error) => {
+    console.error('Google Sign In was unsuccessful. Try again later.', error);
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -41,7 +75,9 @@ const SignUp = () => {
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">{isSignup ? 'Sign up' : 'Sign in'}</Typography>
+        <Typography component="h1" variant="h5">
+          {isSignup ? 'Sign up' : 'Sign in'}
+        </Typography>
         <form className={classes.form} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             {isSignup && (
@@ -51,12 +87,40 @@ const SignUp = () => {
               </>
             )}
             <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-            {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? 'text' : 'password'}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />
+            )}
           </Grid>
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             {isSignup ? 'Sign Up' : 'Sign In'}
           </Button>
+          <GoogleOAuthProvider clientId="692627474953-c434m9lfhreiu9qo7aehmijr681jgiss.apps.googleusercontent.com">
+            <GoogleLogin
+              onSuccess={googleSuccess}
+              onError={googleError}
+              render={(renderProps) => (
+                <Button
+                  className={classes.googleButton}
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  startIcon={<Icon />}
+                  variant="contained"
+                >
+                  Google Sign In
+                </Button>
+              )}
+            />
+          </GoogleOAuthProvider>
+
           <Grid container justifyContent="flex-end">
             <Grid item>
               <Button onClick={switchMode}>
@@ -70,4 +134,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default Auth;
