23f30680cf4d2a335fd9d8b50eb850b843e9c2e4
Anas Ansari <anasansari056789@gmail.com>
2025-03-25 20:57:25 +0530

    Edit Router Path name

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/users', usersRoutes);
+app.use('/user', usersRoutes);
 
 mongoose.connect(process.env.CONNECTION_URL)
   .then(() => {
