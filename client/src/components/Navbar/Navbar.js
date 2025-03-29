b42bbe6d95305f91fca66078b06d4a9f2ffd6c43
Anas Ansari <anasansari056789@gmail.com>
2025-03-29 22:48:58 +0530

    Add new Logo to the App

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -37,27 +37,24 @@ const Navbar = () => {
   }, [location]);
 
   return (
-    <>
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <Link to="/" className={classes.brandContainer}>
+        <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
+        <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
+      </Link>
+      <Toolbar className={classes.toolbar}>
+        {user?.result ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          </div>
+        ) : (
+          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <div className={classes.brandContainer}>
-          <img component={Link} to="/" src={memoriesText} alt="icon" height="45px" />
-          <img className={classes.image} src={memoriesLogo} alt="icon" height="40px" />
-        </div>
-        <Toolbar className={classes.toolbar}>
-          {user?.result ? (
-            <div className={classes.profile}>
-              <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-              <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-              <Button variant="contained" className='' color="secondary" onClick={logout}>Logout</Button>
-            </div>
-          ) : (
-            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-          )}
-        </Toolbar>
-      </AppBar>
-    </>
-  )
-}
-
-export default Navbar
+export default Navbar;
