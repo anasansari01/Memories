aa1d17766b5d3d87d5cc284f6be7ed2b7ae61ab4
Anas Ansari <anasansari056789@gmail.com>
2025-03-29 22:45:47 +0530

    change the string to proper variable to handle error for them

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,10 +1,10 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, FETCH_POST, CREATE, UPDATE, DELETE, LIKE, START_LOADING, END_LOADING } from '../constants/actionTypes';
 
 export default (state = { isLoading: true, posts: [] }, action) => {
   switch (action.type) {
-    case 'START_LOADING':
+    case START_LOADING:
       return { ...state, isLoading: true };
-    case 'END_LOADING':
+    case END_LOADING:
       return { ...state, isLoading: false };
     case FETCH_ALL:
       return {
